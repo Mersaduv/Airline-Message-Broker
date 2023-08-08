5af19cdeea050d582d92404ad0043a0adeef8fc9
mersad karimi <mersad@tooskatech.com>
2023-08-08 21:10:46 +0330

    final commit building a message producer and message consumer

--- FormulaAirline.TicketProcessing/Program.cs
+++ FormulaAirline.TicketProcessing/Program.cs
@@ -1,2 +1,35 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Text;
+using System.Text.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+Console.WriteLine("welcome to ticketing service!");
+
+var factory = new ConnectionFactory()
+{
+    HostName = "localhost",
+    UserName = "user",
+    Password = "mypass",
+    VirtualHost = "/"
+};
+
+var conn = factory.CreateConnection();
+using var channel = conn.CreateModel();
+
+channel.QueueDeclare("bookings", durable: true, exclusive: false);
+
+var consumer = new EventingBasicConsumer(channel);
+
+consumer.Received += (sender, args) =>
+{
+    var body = args.Body.ToArray();
+
+    var message = Encoding.UTF8.GetString(body);
+
+    Console.WriteLine($"New ticket processing is initiated - {message}");
+};
+
+channel.BasicConsume("bookings", true, consumer);
+
+Console.ReadKey();
